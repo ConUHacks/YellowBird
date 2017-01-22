c2b774a54125fabc7fa48e51e548f520185ba04d
NawrasRabbani <nawras.rabbani@mail.mcgill.ca>
2017-01-22 03:12:25 -0500

    functionnality working

--- bot/views.py
+++ bot/views.py
@@ -1,24 +1,39 @@
+from django.template import Context
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Query
 
-from bot.static.bot.py import yellowpages
+import json
+from .static.bot.py import nuance
+from .static.bot.py import yellowpages
+
 
 def query(request):
     return render(request, 'bot/query.html')
 
-# def result(request):
-#     # concept = request.POST['concept'];
-#     # location_x = request.POST['location_x'];
-#     # location_y = request.POST['location_y'];
-#     # Query.objects.create(concept=concept, location_x=location_x, location_y=location_y)
-#
-#     location = location_x + "," + location_y
-#     yellowpages.query_yp(concept,location)
-#
-#     return render(request, 'bot/result.html')
-
 def result(request):
-    chat = request.POST['chat'];
-    print(chat)
-    return render(request, 'bot/result.html')
+    try:
+        chat = request.POST['chat'];
+        print("captured from query input:" + chat)
+    except ValueError:
+        print("Error: nuance query returned invalid value")
+        chat = "food"
+
+    # nuance api call when it works
+    # query_nuance_result = nuance.query_nuance(chat)
+
+    # yellowpages api call
+    query_yellowpages_result = yellowpages.query_yp(chat)
+
+    coords_0 = [float(query_yellowpages_result[0]['centroid'].split(",")[0]), float(query_yellowpages_result[0]['centroid'].split(",")[1])]
+    coords_1 = [float(query_yellowpages_result[1]['centroid'].split(",")[0]), float(query_yellowpages_result[1]['centroid'].split(",")[1])]
+    coords_2 = [float(query_yellowpages_result[2]['centroid'].split(",")[0]), float(query_yellowpages_result[2]['centroid'].split(",")[1])]
+    coords_3 = [float(query_yellowpages_result[3]['centroid'].split(",")[0]), float(query_yellowpages_result[3]['centroid'].split(",")[1])]
+
+    context = {
+        'results_head_0' : { 'name': query_yellowpages_result[0]['businessName'] , 'coords': coords_0},
+        'results_head_1': {'name': query_yellowpages_result[1]['businessName'], 'coords': coords_1},
+        'results_head_2': {'name': query_yellowpages_result[2]['businessName'], 'coords': coords_2},
+        'results_head_3': {'name': query_yellowpages_result[3]['businessName'], 'coords': coords_3},
+    }
+    return render(request, 'bot/result.html', context)
