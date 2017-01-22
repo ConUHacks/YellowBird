435bff602ecbf7e35ede1b9f5c95a25a44864c00
NawrasRabbani <nawras.rabbani@mail.mcgill.ca>
2017-01-21 23:29:48 -0500

    merged first css with backend

--- bot/views.py
+++ bot/views.py
@@ -1,16 +1,19 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Query
 
 from bot.static.bot.py import yellowpages
 
 def query(request):
-    return render(request, 'bot/query2.html')
+    return render(request, 'bot/query.html')
 
 def result(request):
+    concept = request.POST['concept'];
+    location_x = request.POST['location_x'];
+    location_y = request.POST['location_y'];
+    Query.objects.create(concept=concept, location_x=location_x, location_y=location_y)
 
-    concept = "food";
-    location = "45.4754418,-73.5863705";
-
+    location = location_x + "," + location_y
     yellowpages.query_yp(concept,location)
 
     return render(request, 'bot/result.html')
