9b5e14d5bae4232700e757fc6c4069efce5dc935
NawrasRabbani <nawras.rabbani@mail.mcgill.ca>
2017-01-21 20:15:38 -0500

    query and result page added

--- YellowBird/urls.py
+++ YellowBird/urls.py
@@ -2,6 +2,6 @@ from django.conf.urls import include, url
 from django.contrib import admin
 
 urlpatterns = [
-    url(r'^bot/', include('bot.urls')),
+    url(r'^', include('bot.urls')),
     url(r'^admin/', admin.site.urls),
 ]
--- bot/views.py
+++ bot/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-def index(request):
-    return render(request, 'bot/index.html')
+def query(request):
+    return render(request, 'bot/query.html')
+
+def result(request):
+    return render(request, 'bot/result.html')
